28a7e7f048dd4471f17b1b218af5a24fab3cce66
Navyansh Kothari <98050266+navyansh1@users.noreply.github.com>
2024-12-12 09:59:29 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,7 +13,7 @@ const experiences = [
     {
         company: 'Hitwicket',
         period: 'Dec 2024 - Present',
-        description: '🎮 Working on Hitwicket.ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ',
+        description: "🎮 Intern'ing at Hitwicket.ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ",
         icon: hitwicketimg,
         link: 'https://hitwicket.com/',
     },
